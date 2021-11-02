2de83f71b2855f2afc3f567b8b1580bc1168d973
Dmytro Semeruk <deemoon83@gmail.com>
2021-11-02 20:50:21 +0300

    Updates

---
+++ static/js/main.313baa5a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptodo-react"]=this["webpackJsonptodo-react"]||[]).push([[0],{11:function(t,e,c){"use strict";c.r(e);var n=c(1),o=c.n(n),r=c(3),s=c.n(r),i=(c(8),c(9),c(0));function a(){return Object(i.jsx)("div",{className:"App",children:"Hello ToDo"})}s.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.313baa5a.chunk.js.map
